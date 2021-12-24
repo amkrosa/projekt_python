bc33ce5b958a36612a6d5241b7c181b9a21a8abb
amkrosa <annakrasowska2@gmail.com>
2021-12-24 14:26:11 +0100

    added validateName method and nextRow generator method

--- domain/column/Column.py
+++ domain/column/Column.py
@@ -1,6 +1,10 @@
 import abc
 
 class Column(metaclass=abc.ABCMeta):
+    def __init__(self, name: str):
+        if (self.validateName(name)):
+            self.__name = name.strip()
+
     @classmethod
     def __subclasshook__(cls, subclass):
         return (hasattr(subclass, 'load_data_source') and
@@ -8,15 +12,29 @@ class Column(metaclass=abc.ABCMeta):
                 hasattr(subclass, 'extract_text') and
                 callable(subclass.extract_text) or
                 NotImplemented)
-    @classmethod
+
+    @property
     @abc.abstractmethod
-    def getData(cls):
+    def data(self):
         """Get dictionary containing data for specified column"""
         raise NotImplementedError
 
-    @classmethod
+    @data.setter
     @abc.abstractmethod
-    def writeData(cls, data):
+    def data(self, *data):
         """Write dictionary containing data for specified column"""
         raise NotImplementedError
+
+    def validateName(self, name: str) -> bool:
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string")
+        if len(name.strip()) == 0:
+            raise ValueError("Name cannot consist only of whitespaces")
+
+    def nextRow(self):
+        row=0
+        while True:
+            yield row
+            row+=1
+
 
