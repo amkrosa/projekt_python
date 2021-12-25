522348544876dad0ce27bc46cb7b51ded06f1539
amkrosa <annakrasowska2@gmail.com>
2021-12-25 15:59:12 +0100

    Repository and Services (not sure if services will be used?)

---
+++ infrastructure/Repository.py
@@ -0,0 +1,22 @@
+import uuid
+from typing import Dict, Any, TypeVar, Union
+
+from domain.Table import Table
+from domain.column.Column import Column
+
+T = TypeVar("T", bound=Union[Table, Column])
+
+
+class Repository:
+    def __init__(self):
+        self.__repository: Dict[uuid, T] = dict()
+
+    @property
+    def repository(self):
+        return self.__repository
+
+    def add(self, arg):
+        self.repository[uuid.uuid4()] = arg
+
+    def remove(self, arg: uuid):
+        self.repository.pop(arg)
