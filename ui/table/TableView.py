c7f36f2fb61edaae0f09a249007b5ea60723b493
amkrosa <annakrasowska2@gmail.com>
2021-12-27 23:28:37 +0100

    TableView changed to dearpygui

--- ui/table/TableView.py
+++ ui/table/TableView.py
@@ -1,43 +1,45 @@
-import tkinter
-from tkinter import RIGHT, BOTTOM, ttk, X, Y, CENTER, NO
+from uuid import uuid4
+
+from dearpygui.dearpygui import window
+import dearpygui.dearpygui as dpg
 
 
 class TableView:
-    def __init__(self, frame: tkinter.Frame):
-        # scrollbar
-        scroll = tkinter.Scrollbar(frame)
-        scroll.pack(side=RIGHT, fill=Y)
+    def __init__(self, root: window):
+        self.__root = root
+        self.__addTableButton = "addTableButton"
+        self.__tablesTable = "tablesTable"
+        self.__tablesTableRowCount = 0
+        self.__databaseTables()
+        self.__createAddTableButton()
 
-        scroll = tkinter.Scrollbar(frame, orient='horizontal')
-        scroll.pack(side=BOTTOM, fill=X)
+    @property
+    def addTableButton(self):
+        return self.__addTableButton
 
-        table = ttk.Treeview(frame, yscrollcommand=scroll.set, xscrollcommand=scroll.set)
+    @property
+    def tablesTable(self):
+        return self.__tablesTable
 
-        table.pack()
+    def __createAddTableButton(self):
+        dpg.add_button(tag="addTableButton", label="Dodaj tabele", parent="root")
 
-        scroll.config(command=table.yview)
-        scroll.config(command=table.xview)
+    def __databaseTables(self):
+        with self.__root:
+            with dpg.table(tag="tablesTable", header_row=True):
+                dpg.add_table_column(tag="tablesTableNameColumn", label="Name")
+                dpg.add_table_column(tag="tablesTableRowsColumn", label="Rows")
 
-        # define our column
+    def addRow(self, data):
+        uuid = uuid4
+        with dpg.table_row(parent=self.tablesTable):
+            dpg.add_text(data, tag=uuid.__str__())
+        return uuid
 
-        table['columns'] = ('player_id', 'player_name', 'player_Rank', 'player_states', 'player_city')
+    def changeRow(self, tag, data):
+        dpg.set_value(tag, data)
 
-        # format our column
-        table.column("#0", width=0, stretch=NO)
-        table.column("player_id", anchor=CENTER, width=80)
-        table.column("player_name", anchor=CENTER, width=80)
-        table.column("player_Rank", anchor=CENTER, width=80)
-        table.column("player_states", anchor=CENTER, width=80)
-        table.column("player_city", anchor=CENTER, width=80)
-
-        # Create Headings
-        table.heading("#0", text="", anchor=CENTER)
-        table.heading("player_id", text="Id", anchor=CENTER)
-        table.heading("player_name", text="Name", anchor=CENTER)
-        table.heading("player_Rank", text="Rank", anchor=CENTER)
-        table.heading("player_states", text="States", anchor=CENTER)
-        table.heading("player_city", text="States", anchor=CENTER)
-
-        table.insert(parent='', index='end', iid=0, text='',
-                       values=('1', 'Ninja', '101', 'Oklahoma', 'Moore'))
-        table.pack()
+    def setAddTableRegistry(self, handler):
+        with dpg.item_handler_registry(tag="addTableButtonHandler"):
+            dpg.add_item_clicked_handler(callback=handler)
+        dpg.bind_item_handler_registry(self.addTableButton, "addTableButtonHandler")
