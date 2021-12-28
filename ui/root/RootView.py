4f39cc8fd18125df3697d841fcbc68b568d1bfe2
amkrosa <annakrasowska2@gmail.com>
2021-12-28 23:43:09 +0100

    structure

--- ui/root/RootView.py
+++ ui/root/RootView.py
@@ -1,14 +1,11 @@
 import tkinter
 
 import tk
+from dearpygui.dearpygui import window
 
 from ui.table.TableView import TableView
 
 
 class RootView:
-    def __init__(self, root):
-        root.geometry('500x500')
-        root['bg'] = '#AC99F2'
-        frame = tkinter.Frame(root)
-        frame.pack()
-        TableView(frame)
+    def __init__(self, root: window):
+        self.__root = root
