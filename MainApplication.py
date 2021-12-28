0d61db5c86674b1069603362e006ef359414f4e3
amkrosa <annakrasowska2@gmail.com>
2021-12-28 22:31:36 +0100

    removed unnecessary widgets

--- MainApplication.py
+++ MainApplication.py
@@ -1,10 +1,15 @@
-import tkinter
+import dearpygui.dearpygui as dpg
 
-import tk as tk
-
+from ui.root.RootView import RootView
 from ui.root.RootViewModel import RootViewModel
 
 if __name__ == '__main__':
-    root = tkinter.Tk()
-    app = RootViewModel(root)
-    root.mainloop()
+    dpg.create_context()
+    dpg.create_viewport(title='Klient bazy danych', width=800, height=600)
+    root = dpg.window(tag="root")
+    RootViewModel(root)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.set_primary_window("root", True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
