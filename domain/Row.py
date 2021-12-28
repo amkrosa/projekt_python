c1c32794078664750e6c6a1355eeb858a3af18e2
amkrosa <annakrasowska2@gmail.com>
2021-12-28 23:41:34 +0100

    iter and values

--- domain/Row.py
+++ domain/Row.py
@@ -2,5 +2,12 @@ class Row:
     def __init__(self, data):
         self.__data = data
 
+    def __iter__(self):
+        return iter(self.__data)
+
+    @property
+    def values(self):
+        return self.__data.items()
+
     def __getitem__(self, columnName: str):
         return self.__data[columnName]
